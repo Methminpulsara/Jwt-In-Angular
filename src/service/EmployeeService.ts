7f55ea1bc4c03e517edfb5e5f267a5de1fc13eee
Methminpulsara <methminpulsara10@gmail.com>
2025-05-28 22:16:37 +0530

    update employee Service

--- src/service/EmployeeService.ts
+++ src/service/EmployeeService.ts
@@ -6,46 +6,31 @@ import { Observable } from "rxjs";
 @Injectable ({providedIn:'root'})
 
 export default class EmployeeService {
+  constructor(private http:HttpClient){}
 
+  private baseUrl ='http://localhost:8080/api/employee';
 
-  constructor(private http:HttpClient){
-   
-
-    
-  }
-   
   save(employee:Employee):Observable<Employee>{
-
-
-    return this.http.post<Employee>("http://localhost:8080/api/employee/add",employee);
+    return this.http.post<Employee>(`${this.baseUrl}/add`,employee);
   }
 
   update(employeeId:number,employee:Employee):Observable<Employee>{
-      const token = localStorage.getItem("token")
-    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-    return this.http.put<Employee>(`http://localhost:8080/api/employee/update/${employeeId}`, employee);
+    return this.http.put<Employee>(`${this.baseUrl}/update/${employeeId}`,employee);
   }
 
   getAll():Observable<Employee[]>{
-      const token = localStorage.getItem("token")
-    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-  return this.http.get<Employee[]>("http://localhost:8080/api/employee/all");
+  return this.http.get<Employee[]>(`${this.baseUrl}/all`);
   }
 
-  delete(employeeId:number){
-      const token = localStorage.getItem("token")
-    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-    return this.http.delete("http://localhost:8080/api/employee/delete/"+employeeId)
+  delete(employeeId:number):Observable<void>{
+    return this.http.delete<void>(
+      `${this.baseUrl}/delete/${employeeId}`
+    )
   }
 
   searchByName(name:string):Observable<Employee>{
-      const token = localStorage.getItem("token")
-    let headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-      return this.http.get<Employee>("http://localhost:8080/api/employee/search/"+name)
+      return this.http.get<Employee>(
+        `${this.baseUrl}/search/${name}`)
   }
 
 }
